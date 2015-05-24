f66d6b56c20f0509fb32569c79ecf1160eaf75e7
David Walker <david@davidwalker.org>
2015-05-24 02:41:20 +0100

    clean up unused code

--- blorb.cpp
+++ blorb.cpp
@@ -104,8 +104,7 @@ Frame crazy[CRAZY_SIZE] = {
 //
 // slow breathing
 // constant on when picked up
-// 
-
+//
 
 
 LedAnimator led = LedAnimator(redPin,greenPin,bluePin);
@@ -120,45 +119,26 @@ void animate(void);
 void testDiff(void);
 
 void setup() {
- Spark.variable("sensor", &sensorReading, INT);
- Spark.variable("threshold", &threshold, INT);
- Spark.variable("brightness", &brightness, INT);
- Spark.variable("fadeAmount", &fadeAmount, INT);
- Spark.variable("min", &min, INT);
- Spark.variable("max", &max, INT);
- Spark.variable("cycles", &cycles, INT);
+  Spark.variable("sensor", &sensorReading, INT);
+  Spark.variable("threshold", &threshold, INT);
+  Spark.function("setThreshold", setThreshold);
+  pinMode(ledPin, OUTPUT);
+  pinMode(knockSensor, INPUT);
 
- Spark.function("setThreshold", setThreshold);
- Spark.function("setDivLimit", setDivLimit);
- Spark.function("setMin", setMin);
- Spark.function("setMax", setMax);
- pinMode(ledPin, OUTPUT); // declare the ledPin as as OUTPUT
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
 
- pinMode(redPin, OUTPUT);
- pinMode(greenPin, OUTPUT);
- pinMode(bluePin, OUTPUT);
+  Serial.begin(9600);
 
- Serial.begin(9600);       // use the serial port
-
- delay(1000);
- Serial.println("Starting...");
-
- Serial.println(breath[1].time);
- Serial.println(breath[1].color[0]);
- Serial.println(breath[1].color[1]);
- Serial.println(breath[1].color[2]);
- Serial.println("length");
- Serial.println(sizeof(breath));
-
+  delay(1000);
+  Serial.println("Starting...");
 
   led.startAnimation(breath, 3, 1);
   while(led.running()) { 
     delay(10);
     led.step();
   }
-
-  sensorReading = analogRead(knockSensor);
- //testDiff();
 }
 
 void loop() {
@@ -179,35 +159,13 @@ void detectKnock(){
     Serial.println(sensorReading);
   }
 
-  // if the sensor reading is greater than the threshold:
   if (sensorReading <= threshold) {
-    // toggle the status of the ledPin:
     ledState = !ledState;
-    // update the LED pin itself:
     digitalWrite(ledPin, ledState);
-    // send the string "Knock!" back to the computer, followed by newline
-    //trigger = true;
     Serial.println("Knock!");
+    // start monster detection
     led.startAnimation(crazy, CRAZY_SIZE, 1);
   }
-}
-
-void testleds(){
-  Serial.println(greenPin);
-  analogWrite(greenPin, 255);
-  delay(1000);
-  analogWrite(greenPin, 0);
-
-  Serial.println(redPin);
-  analogWrite(redPin, 255);
-  delay(1000);
-  analogWrite(redPin, 0);
-
-  Serial.println(bluePin);
-  analogWrite(bluePin, 255);
-  delay(1000);
-  analogWrite(bluePin, 0);
-
 }
 
 int setThreshold(String s)
@@ -215,61 +173,3 @@ int setThreshold(String s)
   threshold = s.toInt();
   return threshold;
 }
-int setMin(String s)
-{
-  min = s.toInt();
-  return min;
-}
-int setMax(String s)
-{
-  max = s.toInt();
-  return max;
-}
-int setDivLimit(String s)
-{
-  divLimit = s.toInt();
-  return divLimit;
-}
-
-void animate(){
-  divider++;
-  if (divider < divLimit)
-    return;
-  divider = 0;
-
-  brightness += fadeAmount;
-
-  if(brightness <= min || brightness >= max){ 
-    fadeAmount = -fadeAmount;
-    cycles--;
-  }
-
-  analogWrite(redPin, brightness);
-  analogWrite(greenPin, brightness);
-  analogWrite(bluePin, brightness);
-}
-
-
-
-void testDiff(){
-  int currentBrightness = 0;
-  int currentTime = 500;
-
-  int startTime = 0;
-  int targetBrightness = 200;
-  int targetTime = 1000;
-
-  int desiredBrightness = (targetBrightness - currentBrightness) * ((float)currentTime - startTime) / (targetTime-startTime);
-
-  int expected = 100;
-
-  Serial.println("Test results");
-  Serial.println(expected);
-  Serial.println(desiredBrightness);
-  Serial.println(expected == desiredBrightness);
-  Serial.println("end");
-}
-
-
-
-
